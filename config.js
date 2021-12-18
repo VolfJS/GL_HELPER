2d21eb2d351ee35301c452f7addf210c30bdf0ca
Sergey Volkov <sergeyvolkovdev@gmail.com>
2021-12-18 21:23:14 +0300

    Config edit

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 const config = {
     bot_name: "Group Leader Helper", // название бота
     bot_token: "", // токен бота телеграм
-    admins: [], // id админов бота
+    admins: [ 1173871498 ], // id админов бота
     spam_number: 40, // количество сообщений для включения антиспама
     login_db: 'postgres', // логин от базы данных postgres
     password_db: '', // пароль от базы данных postgres
+    admin_chat: 123, // id чата администрации бота
     dir_log: './logs' // директория для логов
 }
 
